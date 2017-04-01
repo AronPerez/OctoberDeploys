9f234f8d02eb87cdacb3cc8cc8830ebdbbb185aa
MagnumP <mphallus0295@gmail.com>
2017-04-01 17:02:30 -0500

    initial checkin

---
+++ plugins/stormbreaker/photos/Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace Stormbreaker\Photos;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
---
+++ plugins/stormbreaker/photos/lang/en/lang.php
@@ -0,0 +1,6 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Photos',
+        'description' => 'Will hold movies in the Backend and store these'
+    ]
+];
